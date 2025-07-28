21cd4da6dd07257c2943044f4b469a20e54a68be
Efe Köseoğlu <62774583+efekoseoglu@users.noreply.github.com>
2025-07-28 17:52:40 +0300

    Update deploy.sh

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -4,4 +4,4 @@ set -e
 docker rm -f node
 docker rmi wunduniik/node-microservice
 docker pull wunduniik/node-microservice
-docker run --rm -d -p 7777:3000 --name node nazmulb/node-microservice
+docker run --rm -d -p 7777:3000 --name node wunduniik/node-microservice
